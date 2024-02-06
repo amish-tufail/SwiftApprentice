5c3e6189c15e7b1fef7c4f6297aa0b2b3e6f44af
Amish <amish.tufail2002@gmail.com>
2024-02-06 21:06:29 +0500

    - Propety Wrappers - Global Property - Type Property

--- SwiftApprentice.playground/Pages/9. Properties.xcplaygroundpage/Contents.swift
+++ SwiftApprentice.playground/Pages/9. Properties.xcplaygroundpage/Contents.swift
@@ -48,6 +48,7 @@ class Hello {
         }
         didSet { // Called after value is changed
             print("NewValue: \(a) is changed from OldValue: \(oldValue)")
+            // In SwiftUI/ Swift you can use this to do some conditional logic and assign value to var.
         }
     }
 }
@@ -58,4 +59,41 @@ print(h.a) // No changes so, 10.
 
 h.a = 100
 
+// Property Wrappers: YT VIDEO
 
+// Global Variable: They are default lazy, no need to write lazy with them. Cannot use Property Wrappers with them.
+
+// TYPE Properties:
+
+// Type properties are like shared features or characteristics that belong to the type itself, not to any single object or instance created from that type. Imagine a classroom where:
+//Instance properties are like individual students' belongings (e.g., a backpack or a pencil case). Each student has their own, and they can be different for everyone.
+//Type properties are like the classroom clock or a shared calendar. No matter who comes into the classroom, the clock and the calendar are the same for everyone and there's only one for all to use.
+
+print("--------")
+
+struct Nice {
+    static var name = "Amish"
+    static var age = 22
+//    var value: String { // Not possible as uses, Type property which is not allowed, non-staic can use non-static values in itself
+//        "\(name) & \(age)"
+//    }
+    static var value: String { // Legal, as static so, can use static
+        "\(name) & \(age)"
+    }
+    
+    var v = "Heee"
+}
+
+var nice = Nice()
+
+// print(hh.name) // Cannot access as type property
+// print(hh.value) // Not even this, as it uses Type propety in itself
+print(nice.v)
+
+print(Nice.age)
+print(Nice.name)
+print(Nice.value)
+
+var e = Nice.init(v: "NICE WORlD!")
+
+print(e.v)
