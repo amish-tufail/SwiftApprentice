4399dec08c4936285adef8a487b0bdbc21f5c450
Amish <amish.tufail2002@gmail.com>
2024-02-06 20:09:02 +0500

    - Property Observer

--- SwiftApprentice.playground/Pages/9. Properties.xcplaygroundpage/Contents.swift
+++ SwiftApprentice.playground/Pages/9. Properties.xcplaygroundpage/Contents.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+// LAZY
 class Student {
     lazy var name = "Amish" // This is isnt created until it is first time accessed
     var age = 22
@@ -12,6 +13,7 @@ print(a.age)
 print(a.name) // Now here it called so, created
 
 
+// COMPUTED
 class Uni {
     var gpa: Double = 10.0
     var calculate: Double {
@@ -34,3 +36,26 @@ print(uni1.calculate) // No set so, 20.0
 
 uni1.calculate = 10.0 // Set value
 print(uni1.calculate) // So, now 30.0
+
+// Property Observer
+
+// They are only for **Stored Properties**
+
+class Hello {
+    var a: Int = 10 {
+        willSet { // Called before value is changed
+            print("OldValue: \(a) is going to be changed to NewValue: \(newValue)")
+        }
+        didSet { // Called after value is changed
+            print("NewValue: \(a) is changed from OldValue: \(oldValue)")
+        }
+    }
+}
+
+var h = Hello()
+
+print(h.a) // No changes so, 10.
+
+h.a = 100
+
+
