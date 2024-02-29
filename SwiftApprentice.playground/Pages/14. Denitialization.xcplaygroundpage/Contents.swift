9b7eda14819234f447f8a79044a838455c02d6fe
Amish <amish.tufail2002@gmail.com>
2024-02-29 20:52:37 +0500

    14. Dinits - How they work and an example

---
+++ SwiftApprentice.playground/Pages/14. Denitialization.xcplaygroundpage/Contents.swift
@@ -0,0 +1,63 @@
+import Foundation
+
+// Deinitializers are only available on class types.
+
+// Class definitions can have at most one deinitializer per class.
+
+/*
+ 
+     deinit {
+     // perform the deinitialization
+     }
+ 
+*/
+
+// Deinitializers are called automatically, just before instance deallocation takes place. You aren’t allowed to call a deinitializer yourself.
+
+// In dinit, you havent dealloacted yet so, you can acces all properties and functions.
+
+class Bank {
+    static var coinsInBank = 10_000
+    static func distribute(coins numberOfCoinsRequested: Int) -> Int {
+        let numberOfCoinsToVend = min(numberOfCoinsRequested, coinsInBank)
+        coinsInBank -= numberOfCoinsToVend
+        return numberOfCoinsToVend
+    }
+    static func receive(coins: Int) {
+        coinsInBank += coins
+    }
+}
+
+class Player {
+    var coinsInPurse: Int
+    init(coins: Int) {
+        coinsInPurse = Bank.distribute(coins: coins)
+    }
+    func win(coins: Int) {
+        coinsInPurse += Bank.distribute(coins: coins)
+    }
+    // Now this, deinit returns the coin whenever it no longer exist that means it is deallocated
+    deinit {
+        Bank.receive(coins: coinsInPurse)
+    }
+}
+
+var playerOne: Player? = Player(coins: 100)
+print("A new player has joined the game with \(playerOne!.coinsInPurse) coins")
+// Prints "A new player has joined the game with 100 coins"
+print("There are now \(Bank.coinsInBank) coins left in the bank")
+// Prints "There are now 9900 coins left in the bank"
+
+
+playerOne!.win(coins: 2_000)
+print("PlayerOne won 2000 coins & now has \(playerOne!.coinsInPurse) coins")
+// Prints "PlayerOne won 2000 coins & now has 2100 coins"
+print("The bank now only has \(Bank.coinsInBank) coins left")
+// Prints "The bank now only has 7900 coins left"
+
+
+playerOne = nil // Here we, nil whichs means no longer exist and before deallocating dinit is called
+print("PlayerOne has left the game")
+// Prints "PlayerOne has left the game"
+print("The bank now has \(Bank.coinsInBank) coins")
+// Prints "The bank now has 10000 coins"
