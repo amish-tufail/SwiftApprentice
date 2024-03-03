edef6c85e2b067557c418f11c1725d57e7a54bf8
Amish <amish.tufail2002@gmail.com>
2024-03-03 20:59:39 +0500

    -  Extensions Complete

--- SwiftApprentice.playground/Pages/19. Extensions.xcplaygroundpage/Contents.swift
+++ SwiftApprentice.playground/Pages/19. Extensions.xcplaygroundpage/Contents.swift
@@ -26,3 +26,76 @@ let oneInch = 25.4.mm
 print("One inch is \(oneInch) meters")
 
 // Extensions can add new computed properties, but they can’t add stored properties, or add property observers to existing properties.
+
+// INIT'S
+
+struct Size {
+    var width = 0.0, height = 0.0
+}
+struct Point {
+    var x = 0.0, y = 0.0
+}
+struct Rect {
+    var origin = Point()
+    var size = Size()
+}
+
+extension Rect {
+    init(center: Point, size: Size) {
+        let originX = center.x - (size.width / 2)
+        let originY = center.y - (size.height / 2)
+        self.init(origin: Point(x: originX, y: originY), size: size)
+    }
+}
+
+// Methods
+
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+
+// Mutating Instance Methods
+
+extension Int {
+    mutating func square() {
+        self = self * self
+    }
+}
+var someInt = 3
+someInt.square() // someInt is now 9
+
+// Subscripts
+
+extension Int {
+    subscript(digitIndex: Int) -> Int {
+        var decimalBase = 1
+        for _ in 0..<digitIndex {
+            decimalBase *= 10
+        }
+        return (self / decimalBase) % 10
+    }
+}
+746381295[0]
+// returns 5
+
+// Nested Types
+
+extension Int {
+    enum Kind {
+        case negative, zero, positive
+    }
+    var kind: Kind {
+        switch self {
+        case 0:
+            return .zero
+        case let x where x > 0:
+            return .positive
+        default:
+            return .negative
+        }
+    }
+}
