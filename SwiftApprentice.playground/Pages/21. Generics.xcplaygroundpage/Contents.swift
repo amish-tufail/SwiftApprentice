bf304928c1b77d5780e1baaf8fba3754e71e05dc
Amish <amish.tufail2002@gmail.com>
2024-03-10 16:33:57 +0500

    - Generic Type and Extension

--- SwiftApprentice.playground/Pages/21. Generics.xcplaygroundpage/Contents.swift
+++ SwiftApprentice.playground/Pages/21. Generics.xcplaygroundpage/Contents.swift
@@ -14,3 +14,34 @@ func swapTwoValues<T>(_ a: inout T, _ b: inout T) {
     a = b
     b = temporaryA
 }
+
+// Generic TYPES
+// These are custom classes, structures, and enumerations that can work with any type.
+
+struct IntStack {
+    var items: [Int] = []
+    mutating func push(_ item: Int) {
+        items.append(item)
+    }
+    mutating func pop() -> Int {
+        return items.removeLast()
+    }
+}
+
+
+struct Stack<Element> {
+    var items: [Element] = []
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeLast()
+    }
+}
+
+// Generic Type is auto extended in extension so, we dont need to specify a Generic Type here
+extension Stack {
+    var topItem: Element? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+}
